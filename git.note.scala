321545f7bcebd9f5a216c21474109abf5735fa18
lildata <frederic.moser@gmail.com>
2015-07-05 16:44:36 +0200

    Init

---
+++ git.note.scala
@@ -0,0 +1,8 @@
+Object Commands {
+	val commands = Map(
+		"git init" -> "Initialize a repository and create .git/",
+		"git status" -> "status of the sync, if there is something to comit...",
+		"git add git.note.scala" -> "Add the file to the Staging Area"
+		"git commit -m \"store the staged changes with a message\""
+	)
+}
